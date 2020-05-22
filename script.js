51b53f93afee6e53dcae1c587d6c06bba8301e60
dhavalsingh-eGov <dhaval.singh@egovernments.org>
2020-05-22 23:48:47 +0530

    log

---
+++ script.js
@@ -0,0 +1,148 @@
+var Data = {
+  data: [
+    {
+      date: "15/05/22",
+      weight: 84,
+      cals: 2674,
+      cigs: 4,
+    },
+    {
+      date: "16/05/22",
+      weight: 83.4,
+      cals: 1900,
+      cigs: 3,
+    },
+    {
+      date: "17/05/22",
+      weight: 83.9,
+      cals: 2100,
+      cigs: 4.5,
+    },
+    {
+      date: "18/05/22",
+      weight: 83.2,
+      cals: 1900,
+      cigs: 2,
+    },
+    {
+      date: "19/05/22",
+      weight: 84.3,
+      cals: 2400,
+      cigs: 5,
+    },
+  ],
+};
+
+var labels = Data.data.map(function (e) {
+  return e.date;
+});
+
+var weights = Data.data.map(function (e) {
+  return e.weight;
+});
+
+var cal = Data.data.map(function (e) {
+  return e.cals;
+});
+
+var cig = Data.data.map(function (e) {
+  return e.cigs;
+});
+new Chart(document.getElementById("mixed-chart"), {
+  type: "bar",
+  data: {
+    labels: labels,
+    datasets: [
+      {
+        label: "Calories",
+        type: "line",
+        borderColor: "#8e5ea2",
+        data: cal,
+        fill: false,
+        yAxisID: "calories",
+      },
+      {
+        label: "Weight",
+        type: "line",
+        borderColor: "#3e95cd",
+        data: weights,
+        fill: false,
+        yAxisID: "weight",
+      },
+      {
+        label: "Ciggrates",
+        type: "bar",
+        backgroundColor: "rgba(0,0,0,0.2)",
+        data: cig,
+        yAxisID: "cigs",
+        barThickness: 50,
+      },
+    ],
+  },
+  options: {
+    title: {
+      display: true,
+      text: "Fitness Log",
+    },
+    legend: { display: true },
+    scales: {
+      yAxes: [
+        {
+          id: "calories",
+          ticks: {
+            beginAtZero: false,
+            suggestedMin: 1200,
+          },
+          scaleLabel: {
+            display: true,
+            labelString: "Calories(Kcal)",
+          },
+          gridLines: {
+            color: "rgba(0, 0, 0, 0)",
+            zeroLineColor: "black",
+          },
+        },
+        {
+          id: "weight",
+          position: "right",
+          ticks: {
+            beginAtZero: false,
+            suggestedMin: 60,
+          },
+          scaleLabel: {
+            display: true,
+            labelString: "Weight(Kg)",
+          },
+          gridLines: {
+            color: "rgba(0, 0, 0, 0)",
+            zeroLineColor: "black",
+          },
+        },
+        {
+          id: "cigs",
+          ticks: {
+            beginAtZero: true,
+            display: false,
+          },
+          scaleLabel: {
+            display: false,
+            labelString: "Ciggs/Day",
+          },
+          gridLines: {
+            color: "rgba(0, 0, 0, 0)",
+            zeroLineColor: "black",
+            display: false,
+          },
+        },
+      ],
+      xAxes: [
+        {
+          gridLines: {
+            display: false,
+            zeroLineColor: "black",
+          },
+        },
+      ],
+    },
+  },
+});
